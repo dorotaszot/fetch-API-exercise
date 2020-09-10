97830b0d04d1ce41799b94d3d6e3b01fe1df5c16
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-09-10 16:34:31 +0200

    getExternalAPI done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 document.getElementById('button1').addEventListener('click', getText);
 document.getElementById('button2').addEventListener('click', getJSON);
+document.getElementById('button3').addEventListener('click', getExternalAPI);
 
 // Get locat text file data
 function getText() {
@@ -32,3 +33,18 @@ function getJSON() {
     })
 }
 
+// Get external API data
+function getExternalAPI() {
+  fetch("https://api.github.com/users")
+    .then(response => response.json())
+    .then(users => {
+      let output = '';
+      users.forEach(function (user) {
+        output += `
+      <li>${user.login}</li>
+      `
+      })
+      document.getElementById('output').innerHTML = `<p>GitHub Users:</p> ${output}`;
+
+    })
+}
