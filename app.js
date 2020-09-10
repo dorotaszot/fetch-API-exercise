4ca4360cef66e6b49179c5cae2bb9944871afa9c
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-09-10 16:19:25 +0200

    getJSON done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-document.getElementById('button1').addEventListener('click', getText);
 
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJSON);
+
+// Get locat text file data
 function getText() {
   fetch('test.txt')
     .then(function (response) {
@@ -7,6 +10,25 @@ function getText() {
     })
     .then(function (data) {
       document.getElementById('output').innerText = data;
-      // console.log(data);
     });
 };
+
+// Get local JSON data
+function getJSON() {
+  fetch('posts.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (posts) {
+      let output = '';
+      posts.forEach(function (post) {
+        output += `
+        <li>${post.title}</li>
+        <li>${post.body}</li>
+        `
+      })
+
+      document.getElementById('output').innerHTML = output;
+    })
+}
+
